393f4867319aa1c35fd6307961613dd0e8345d18
Sean Sundberg <seansund@us.ibm.com>
2023-03-24 10:52:08 -0500

    Update metadata dependencies for mas core (#10)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,3 +1,13 @@
+module "wait_for_mas_core" {
+  source = "github.com/cloud-native-toolkit/terraform-util-mas-core-ready"
+
+  cluster_config_file = module.dev_cluster.config_file_path
+  entitlement_secret_name = module.mas_core.entitlement_secret_name
+  core_namespace = module.mas_core.core_namespace
+  mas_instance_id = module.mas_core.mas_instance_id
+  mas_workspace_id = module.mas_core.mas_workspace_id
+}
+
 module "gitops_module" {
   source = "./module"
 
@@ -6,10 +16,10 @@ module "gitops_module" {
   kubeseal_cert = module.gitops.sealed_secrets_cert
   server_name = module.gitops.server_name
 
-  core_namespace = module.mas_core.namespace
-  ibm_entitlement_secret = module.mas_core.entitlement_secret_name
-  mas_instance_id = module.mas_core.mas_instance_id
-  mas_workspace_id = module.mas_core.mas_workspace_id
+  core_namespace = module.wait_for_mas_core.core_namespace
+  ibm_entitlement_secret = module.wait_for_mas_core.entitlement_secret_name
+  mas_instance_id = module.wait_for_mas_core.mas_instance_id
+  mas_workspace_id = module.wait_for_mas_core.mas_workspace_id
 
   db2_meta_storage_class = module.storage_manager.rwx_storage_class
   db2_data_storage_class = module.storage_manager.block_storage_class
